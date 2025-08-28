2f9cd8aa4d3b305a9004c3b2eb3c4e5aadb85df9
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-08-28 08:01:53 -0400

    Added Dialog component to Tabs

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -28,8 +28,18 @@ import {
   AvatarImage,
   Badge,
   Button,
+  Checkbox,
   Combobox,
   Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  Input,
+  Label,
   Skeleton,
   Table,
   TableBody,
@@ -192,7 +202,64 @@ const Tabs = () => {
     ),
     dialog: (
       <div className="mt-5 flex justify-center">
-        <Dialog />
+        <p>Contains Dialog with Label, Input and Checkbox component</p>
+        <Dialog>
+          <form>
+            <DialogTrigger asChild>
+              <Button variant="outline">Open Dialog</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you&apos;re
+                  done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="flex flex-col gap-4 py-2">
+                <div className="flex flex-col gap-3">
+                  <Label htmlFor="name-1">Name</Label>
+                  <Input
+                    type="name"
+                    placeholder="Pedro Duarte"
+                    className="col-span-3"
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <Label htmlFor="username-1">Username</Label>
+                  <Input
+                    type="username"
+                    placeholder="@peduarte"
+                    className="col-span-3"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col items-start space-y-4">
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="toggle" disabled />
+                  <Label
+                    htmlFor="toggle"
+                    className="text-sm text-muted-foreground"
+                  >
+                    Email notifications (disabled)
+                  </Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="terms" defaultChecked />
+                  <Label htmlFor="terms" className="text-sm">
+                    I agree to the terms and conditions
+                  </Label>
+                </div>
+              </div>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                <Button type="submit">Save changes</Button>
+              </DialogFooter>
+            </DialogContent>
+          </form>
+        </Dialog>
       </div>
     ),
     combobox: (
